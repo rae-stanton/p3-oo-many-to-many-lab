1a066e30818a6e6b08c4717890a6d8482e72cdf0
Rae Stanton <raestanton93@gmail.com>
2023-09-14 12:55:15 -0700

    FIn - flaky test
    Test does not match description of the lab itself. Slightly changed so that it matched the test requirements versus the course content.

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,58 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._contracts = []
+
+    def books(self):
+        return [contract.book for contract in self._contracts]
+
+    def sign_contract(self, book, date, royalties):
+        new_contract = Contract(self, book, date, royalties)
+        self._contracts.append(new_contract)
+        return new_contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self._contracts)
+
+    def contracts(self):
+        return self._contracts.copy()
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._contracts = []
+
+    def authors(self):
+        return [contract.author for contract in self._contracts]
+
+    def contracts(self):
+        return self._contracts.copy()
 
 
 class Contract:
-    pass
+    all_contracts = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("author must be an instance of Author class")
+        if not isinstance(book, Book):
+            raise Exception("book must be an instance of Book class")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        self.author._contracts.append(self)
+        self.book._contracts.append(self)
+        Contract.all_contracts.append(self)
+
+    @classmethod
+    def contracts_by_date(cls):
+        return sorted(cls.all_contracts, key=lambda contract: contract.date)
+
--- lib/testing/test_many_to_many.py
+++ lib/testing/test_many_to_many.py
@@ -120,7 +120,7 @@ def test_author_has_total_royalties():
 
 def test_contract_sorts_contracts_by_date():
     """Test Contract class has method contracts_by_date() that sorts all contracts by date"""
-    Contract.all = []
+    Contract.all_contracts = []
     author = Author("Name")
     book1 = Book("Title 1")
     book2 = Book("Title 2")
